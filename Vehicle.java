9754c94e18af879e0578dad5d0b877aa9585e7ba
Ethan Lee <40707681+GreenPlane@users.noreply.github.com>
2019-02-01 12:01:16 -0800

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,29 +1,51 @@
-import java.lang.Math;
+package ap2014.t2.lesson4;
 
+/**
+ Created by acorn on 1/16/15.
+ 
+ Create a class called Vehicle that simulates a car moving along a 40 block stretch of road.
+
+Your class will build a vehicle and keep track of its location on the road. Location values may range from -20 to 20. A location value of 0 represents block 0, a location value of 1 represents block 1, a location value of 2 represents block 2, etc. If the user tries to move the vehicle beyond block +20 or -20, set the location to +/- 20 respectively.
+
+In previous exercises, we had a requirement that you name your class Main. In this exercise, you must name your class Vehicle.
+ **/
 public class Vehicle {
-    private int location = (int)(Math.random()*40)-20;
+  private int location;
 
-    public Vehicle(){
-        location = 0;
+  public Vehicle() {
+    this(0);
+  }
+
+  public Vehicle(int loc) {
+    if (loc >= -20 && loc <= 20) {
+      this.location = loc;
+    } else {
+      this.location = 0;
     }
+  }
 
-    public Vehicle(int loc){
-        if (loc >= -20 && loc <= 20){
-            location = loc;
-        }
-        else{
-            location = 0;
-        }
+  public void forward() {
+    if (location < 20) {
+      ++location;
     }
+  }
 
-    public void forward(){
-        if (location >= 20){
-            System.out.println("Cannot move past 20.");
-        }
-        else{
-            location++;
-        }
+  public void backward() {
+    if (location > -20) {
+      --location;
     }
+  }
 
-    public 
+  public int getLocation() {
+    return location;
+  }
+
+  @Override
+  public String toString() {
+    String result = "";
+    for (int i=-20; i<location; i++) {
+      result += " ";
+    }
+    return result + "@";
+  }
 }
